f3f702cedbbf2dd9f03c51888a8b116f9e70d706
zohaib-eng <zohaibdon41@gmail.com>
2022-07-24 14:22:59 +0500

    first project

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿
+namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtitem = new System.Windows.Forms.TextBox();
+            this.txtrate = new System.Windows.Forms.TextBox();
+            this.txttotal = new System.Windows.Forms.TextBox();
+            this.txtgrandtotal = new System.Windows.Forms.TextBox();
+            this.checkBox1Garments = new System.Windows.Forms.CheckBox();
+            this.checkBox2jewllery = new System.Windows.Forms.CheckBox();
+            this.checkBox3others = new System.Windows.Forms.CheckBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnresult = new System.Windows.Forms.Button();
+            this.btnprint = new System.Windows.Forms.Button();
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Goudy Stout", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(201, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(381, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculator";
+            // 
+            // txtitem
+            // 
+            this.txtitem.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtitem.Location = new System.Drawing.Point(67, 96);
+            this.txtitem.Multiline = true;
+            this.txtitem.Name = "txtitem";
+            this.txtitem.Size = new System.Drawing.Size(128, 48);
+            this.txtitem.TabIndex = 101;
+            this.txtitem.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtitem.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.txtitem.Enter += new System.EventHandler(this.txtitem_Enter);
+            this.txtitem.Leave += new System.EventHandler(this.txtitem_Leave);
+            // 
+            // txtrate
+            // 
+            this.txtrate.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtrate.Location = new System.Drawing.Point(233, 96);
+            this.txtrate.Multiline = true;
+            this.txtrate.Name = "txtrate";
+            this.txtrate.Size = new System.Drawing.Size(128, 48);
+            this.txtrate.TabIndex = 102;
+            this.txtrate.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtrate.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            this.txtrate.Enter += new System.EventHandler(this.txtrate_Enter);
+            this.txtrate.Leave += new System.EventHandler(this.txtrate_Leave);
+            // 
+            // txttotal
+            // 
+            this.txttotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txttotal.Location = new System.Drawing.Point(399, 96);
+            this.txttotal.Multiline = true;
+            this.txttotal.Name = "txttotal";
+            this.txttotal.Size = new System.Drawing.Size(128, 48);
+            this.txttotal.TabIndex = 103;
+            this.txttotal.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txttotal.Enter += new System.EventHandler(this.txttotal_Enter);
+            this.txttotal.Leave += new System.EventHandler(this.txttotal_Leave);
+            // 
+            // txtgrandtotal
+            // 
+            this.txtgrandtotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtgrandtotal.Location = new System.Drawing.Point(565, 96);
+            this.txtgrandtotal.Multiline = true;
+            this.txtgrandtotal.Name = "txtgrandtotal";
+            this.txtgrandtotal.Size = new System.Drawing.Size(128, 48);
+            this.txtgrandtotal.TabIndex = 104;
+            this.txtgrandtotal.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtgrandtotal.Enter += new System.EventHandler(this.txtgrandtotal_Enter);
+            this.txtgrandtotal.Leave += new System.EventHandler(this.txtgrandtotal_Leave);
+            // 
+            // checkBox1Garments
+            // 
+            this.checkBox1Garments.AutoSize = true;
+            this.checkBox1Garments.Location = new System.Drawing.Point(96, 177);
+            this.checkBox1Garments.Name = "checkBox1Garments";
+            this.checkBox1Garments.Size = new System.Drawing.Size(71, 17);
+            this.checkBox1Garments.TabIndex = 105;
+            this.checkBox1Garments.Text = "Garments";
+            this.checkBox1Garments.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2jewllery
+            // 
+            this.checkBox2jewllery.AutoSize = true;
+            this.checkBox2jewllery.Location = new System.Drawing.Point(96, 209);
+            this.checkBox2jewllery.Name = "checkBox2jewllery";
+            this.checkBox2jewllery.Size = new System.Drawing.Size(69, 17);
+            this.checkBox2jewllery.TabIndex = 106;
+            this.checkBox2jewllery.Text = "Jewellery";
+            this.checkBox2jewllery.UseVisualStyleBackColor = true;
+            this.checkBox2jewllery.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3others
+            // 
+            this.checkBox3others.AutoSize = true;
+            this.checkBox3others.Location = new System.Drawing.Point(96, 241);
+            this.checkBox3others.Name = "checkBox3others";
+            this.checkBox3others.Size = new System.Drawing.Size(57, 17);
+            this.checkBox3others.TabIndex = 107;
+            this.checkBox3others.Text = "Others";
+            this.checkBox3others.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(96, 280);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(598, 221);
+            this.dataGridView1.TabIndex = 110;
+            // 
+            // btnresult
+            // 
+            this.btnresult.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnresult.Location = new System.Drawing.Point(593, 177);
+            this.btnresult.Name = "btnresult";
+            this.btnresult.Size = new System.Drawing.Size(100, 39);
+            this.btnresult.TabIndex = 108;
+            this.btnresult.Text = "Result";
+            this.btnresult.UseVisualStyleBackColor = true;
+            this.btnresult.Click += new System.EventHandler(this.btnresult_Click);
+            // 
+            // btnprint
+            // 
+            this.btnprint.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnprint.Location = new System.Drawing.Point(593, 225);
+            this.btnprint.Name = "btnprint";
+            this.btnprint.Size = new System.Drawing.Size(100, 39);
+            this.btnprint.TabIndex = 109;
+            this.btnprint.Text = "Print";
+            this.btnprint.UseVisualStyleBackColor = true;
+            this.btnprint.Click += new System.EventHandler(this.btnprint_Click);
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.UseEXDialog = true;
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 531);
+            this.Controls.Add(this.btnprint);
+            this.Controls.Add(this.btnresult);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.checkBox3others);
+            this.Controls.Add(this.checkBox2jewllery);
+            this.Controls.Add(this.checkBox1Garments);
+            this.Controls.Add(this.txtgrandtotal);
+            this.Controls.Add(this.txttotal);
+            this.Controls.Add(this.txtrate);
+            this.Controls.Add(this.txtitem);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtitem;
+        private System.Windows.Forms.TextBox txtrate;
+        private System.Windows.Forms.TextBox txttotal;
+        private System.Windows.Forms.TextBox txtgrandtotal;
+        private System.Windows.Forms.CheckBox checkBox1Garments;
+        private System.Windows.Forms.CheckBox checkBox2jewllery;
+        private System.Windows.Forms.CheckBox checkBox3others;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnresult;
+        private System.Windows.Forms.Button btnprint;
+        private System.Windows.Forms.PrintDialog printDialog1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+    }
+}
+
---
+++ Calculator/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection  con = new MySqlConnection("server=localhost;user id=root;database=items_calculator;" +
+       "persistsecurityinfo=True;allowuservariables=True");
+        MySqlCommand cmd;
+        MySqlDataAdapter sda;
+        int i = 0;
+        String Choice = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Start();
+            Display();
+        }
+
+        public void Start()
+        {
+            txtitem.Text = "Items";
+            txtitem.ForeColor = Color.LightGray;
+
+            txtrate.Text = "Rate";
+            txtrate.ForeColor = Color.LightGray;
+
+            txttotal.Text = "Total";
+            txttotal.ForeColor = Color.LightGray;
+
+            txtgrandtotal.Text = "G.Total";
+            txtgrandtotal.ForeColor = Color.LightGray;
+        }
+
+        private void txtitem_Enter(object sender, EventArgs e)
+        {
+            if (txtitem.Text == "Items")
+            {
+                txtitem.Text = "";
+                txtitem.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtitem_Leave(object sender, EventArgs e)
+        {
+            if (txtitem.Text == "" || txtitem.Text == "Items")
+            {
+                txtitem.Text = "Items";
+                txtitem.ForeColor = Color.LightGray;
+            }
+        }
+
+        private void txtrate_Enter(object sender, EventArgs e)
+        {
+            if (txtrate.Text == "Rate")
+            {
+                txtrate.Text = "";
+                txtrate.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtrate_Leave(object sender, EventArgs e)
+        {
+            if (txtrate.Text == "" || txtrate.Text == "Rate")
+            {
+                txtrate.Text = "Rate";
+                txtrate.ForeColor = Color.LightGray;
+            }
+        }
+
+        private void txttotal_Enter(object sender, EventArgs e)
+        {
+            if (txttotal.Text == "Total")
+            {
+                txttotal.Text = "";
+                txttotal.ForeColor = Color.Black;
+            }
+        }
+
+        private void txttotal_Leave(object sender, EventArgs e)
+        {
+            if (txttotal.Text == "" || txttotal.Text == "Total")
+            {
+                txttotal.Text = "Total";
+                txttotal.ForeColor = Color.LightGray;
+            }
+        }
+
+        private void txtgrandtotal_Enter(object sender, EventArgs e)
+        {
+            if (txtgrandtotal.Text == "G.Total")
+            {
+                txtgrandtotal.Text = "";
+                txtgrandtotal.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtgrandtotal_Leave(object sender, EventArgs e)
+        {
+            if (txtgrandtotal.Text == "" || txtgrandtotal.Text == "G.Total")
+            {
+                txtgrandtotal.Text = "G.Total";
+                txtgrandtotal.ForeColor = Color.LightGray;
+            }
+        }
+
+        private void btnresult_Click(object sender, EventArgs e)
+        {
+            if (checkBox1Garments.Checked == true)
+            {
+                Choice = "Garment";
+            }else if(checkBox2jewllery.Checked==true)
+            {
+                Choice = "Jewellery";
+            }else if(checkBox3others.Checked==true)
+            {
+                Choice = "Others";
+            }
+            else
+            {
+                Choice = "None";
+            }
+
+            try
+            {
+                cmd = new MySqlCommand("insert into items(Total_items, Items_Value, Total, Grand_Total,Choice) values('" + txtitem.Text + "', '" + txtrate.Text + "', '" + txttotal.Text + "', '" + txtgrandtotal.Text + "','"+Choice+"')", con);
+                con.Open();
+                cmd.Parameters.AddWithValue("@Total_items", txtitem.Text);
+                cmd.Parameters.AddWithValue("@Items_Value", txtrate.Text);
+                cmd.Parameters.AddWithValue("@Total", txttotal.Text);
+                cmd.Parameters.AddWithValue("@Grand_Total", txtgrandtotal.Text);
+                //cmd.Parameters.AddWithValue("@Choice", Choicecheck.Text);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Record Inserted Successfully");
+            }
+            catch
+            {
+                MessageBox.Show("Record Inserted Failed");
+            }
+
+        }
+
+        public void Display()
+        {
+            con.Open();
+            DataTable dt = new DataTable();
+            sda = new MySqlDataAdapter("select * from items", con);
+            sda.Fill(dt);
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
+
+        private void btnprint_Click(object sender, EventArgs e)
+        {
+            printDialog1.Document = printDocument1;
+            if (printDialog1.ShowDialog() == DialogResult.OK)
+            {
+                printDocument1.Print();
+            }
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            Bitmap dataGridViewBitmap = new Bitmap(this.dataGridView1.Width, this.dataGridView1.Height);
+
+            dataGridView1.DrawToBitmap(dataGridViewBitmap, new System.Drawing.Rectangle(50, 50, this.dataGridView1.Width, this.dataGridView1.Height));
+
+            e.Graphics.DrawImage(dataGridViewBitmap, 50, 50);
+
+        }
+    }
+}
