07f785347d2e452dd0a2326c489883fd267e042b
zohaib-eng <zohaibdon41@gmail.com>
2022-07-24 15:53:36 +0500

    fourth project

---
+++ Datafromviewtocontroller/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Datafromviewtocontroller.Models;
+
+namespace Datafromviewtocontroller.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public String Postusingparameters(String firstname,String lastname)
+        {
+            return "From Parameters - " + firstname + "," + lastname;
+        }
+
+        [HttpPost]
+        public String Postusingruqest(String firstname, String lastname)
+        {
+            String firstName = Request["firstname"];
+            String lastName = Request["lastname"];
+            return "From Ruqest - " + firstname + "," + lastname;
+        }
+
+        public String PostusingFormCollection(FormCollection form)
+        {
+            String firstName = form["firstname"];
+            String lastName = form["lastname"];
+            return "From Form Collection - " + firstName + "," + lastName;
+        }
+
+
+        public String PostusingBinding(Employee emp)
+        {
+            
+            return "From Form Binding - " + emp.firstName + "," + emp.lastName;
+        }
+    }
+}
---
+++ Datafromviewtocontroller/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Datafromviewtocontroller.Models
+{
+    public class Employee
+    {
+        public String firstName { get; set; }
+
+        public String lastName { get; set; }
+    }
+}
