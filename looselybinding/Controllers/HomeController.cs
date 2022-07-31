da21a6ee34e4755dae3c2c0b1aeb6c5fbda48182
zohaib-eng <Zohaibdon41@gmail.com>
2022-07-31 15:35:14 +0500

    sixth project

---
+++ looselybinding/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using looselybinding.Models;
+
+namespace looselybinding.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Submitdata(Employee emp)
+        {
+            if (ModelState.IsValid)
+            {
+                return View();
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ looselybinding/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace looselybinding.Models
+{
+    public class Employee
+    {
+        [Required(ErrorMessage ="please enter your first name")]
+        public String Firstname { get; set; }
+        [Required(ErrorMessage = "please enter your last name")]
+        public String Lastname { get; set; }
+        [Required(ErrorMessage = "please enter your valid email")]
+        [EmailAddress]
+        public String Email { get; set; }
+        [Required(ErrorMessage = "please enter your age")]
+        [Range(0,100)]
+        public int Age { get; set; }
+    }
+}
